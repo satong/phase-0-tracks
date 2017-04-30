e7e417a07f0595212639ddaf2116919185e5d1af
satong <stephanietong@gmail.com>
2017-04-30 12:21:44 +0300

    Add code for release 1 of module 5.6

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -13,3 +13,20 @@ end
 
 # arr = [42, 89, 23, 1]
 # search_array(arr, 24)
+
+# Release 1: calculate fibonacci numbers
+def fib(num)
+  # Assume sequence always starts with 0 and 1
+  sequence = [0,1]
+  if num > 1
+    until sequence.length == num
+      # Add in next number
+      next_number = sequence[-1]+sequence[-2]
+      # Insert number into sequence
+      sequence << next_number
+    end
+    sequence
+  else
+    p "Please enter a number greater than 1"
+  end
+end
