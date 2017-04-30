e36122a66428093eefce51adfcd32392fd3f4b73
satong <stephanietong@gmail.com>
2017-04-30 07:06:13 +0300

    Correct errors in naming inconsistencies

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -10,7 +10,7 @@ p "Please enter the client's age:"
 client_info[:age] = gets.chomp.to_i
 
 p "Is this an urgent project? y/n"
-if gets.chomp.downcase = "y"
+if gets.chomp.downcase == "y"
   client_info[:urgency] = true
 else
   client_info[:urgency] = false
@@ -21,7 +21,7 @@ p client_info
 
 p "Do you need to update anything? Please enter the name of the field you'd like to update or 'none' if you don't need to update anything"
 
-update = gets.chomp.downcase!
+update = gets.chomp.downcase
 
 if update == "none"
   p "Thank you. End."
@@ -29,13 +29,13 @@ else
   p "Please enter the updated value"
   updated_value = gets.chomp
 
-  if update == "age" or updated == "num_child"
+  if update == "age" or update == "num_child"
     updated_value = updated_value.to_i
   end
 
-  client_info[update.to_sym] = value
+  client_info[update.to_sym] = updated_value
 
   p "Here is your updated client profile:"
-  p client
+  p client_info
 
 end
