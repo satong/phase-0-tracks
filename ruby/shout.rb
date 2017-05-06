d89e9873fbde0e29be0f30cb2b9715981bc2675b
satong <stephanietong@gmail.com>
2017-05-06 18:31:07 +0300

    Convert Shout into Mixin module for release 3 of module 6.4

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 # Release 1: Write a Simple Module
 
 module Shout
@@ -14,3 +15,32 @@ end
 # Release 1 driver code
 p Shout.yell_angrily("AHH")
 p Shout.yelling_happily("YAY")
+=end
+
+# Release 3: Convert a Standalone Module to a Mixin
+module Shout
+  def yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def yelling_happily(words)
+    words + "!!!" + " :)"
+  end
+end
+
+class Children
+  include Shout
+end
+
+class Parents
+  include Shout
+end
+
+# Release 3 driver code
+child = Children.new
+p child.yell_angrily("I hate school")
+p child.yelling_happily("I love candy")
+
+parent = Parents.new
+p parent.yell_angrily("I hate work")
+p parent.yelling_happily("I love vacation")
