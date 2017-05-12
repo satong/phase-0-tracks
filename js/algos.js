34d79ca87a9b649c145f8428198aadd1fe791625
satong <stephanietong@gmail.com>
2017-05-12 15:39:26 -0400

    Add release 1 function code to find key-value match across two objects and driver code

--- js/algos.js
+++ js/algos.js
@@ -7,7 +7,7 @@
   // longestElement variable will be updated to whichever is the longest element
 // output: the variable longestElement
 
-function longestPhrase(array){
+function longestPhrase(array) {
   var longestElement = array[0];
   for (var i = 1; i < array.length; i++) {
     if (longestElement.length < array[i].length) {
@@ -17,6 +17,31 @@ function longestPhrase(array){
   return longestElement;
 }
 
+// Release 1: Find a Key-Value Match
+// write a function that takes two objects and checks to see if the objects share at least one key-value pair
+// input: object 1, object 2
+// take object 1 and iterate through each key-value pair
+// determine if the keys in object 1 are valid
+// RETURN true if value of object 1 key == value of the same key in object 2
+// else return false
+
+function keyvalueMatch(object1, object2) {
+  for (var k in object1) {
+    if (object1.hasOwnProperty(k)) {
+      if (object1[k] == object2[k]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+
 // Release 0 driver code:
 var array = ["hatchet", "rations", "water jug", "binoculars","shotgun", "compass", "CB radio", "batteries"];
 console.log(longestPhrase(array));
+
+// Release 1 driver code:
+var object1 = {name: "Steven", age: 54};
+var object2 = {name: "Tamir", age: 54};
+console.log(keyvalueMatch(object1, object2));
