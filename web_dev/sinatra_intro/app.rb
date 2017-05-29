ae2481ac5fd0b47939f1ab1ab63db47a0fa2b676
satong <stephanietong@gmail.com>
2017-05-29 10:41:07 -0700

    Add code for release 0 of module 9.4

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -8,9 +8,9 @@ db.results_as_hash = true
 # write a basic GET route
 # add a query parameter
 # GET /
-get '/' do
-  "#{params[:name]} is #{params[:age]} years old."
-end
+# get '/' do
+  # "#{params[:name]} is #{params[:age]} years old."
+# end
 
 # write a GET route with
 # route parameters
@@ -43,4 +43,35 @@ end
 get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
+end
+
+# Release 0
+# A /contact route that displays an address
+get '/contact' do
+	"You can find us at: <br>4242 End of the World Road <br>Outer Rims of the Galaxy, Galaxy"
+end
+# A /great_job route that can take a person's name as a query parameter (not a route parameter) and say "Good job, [person's name]!". If the query parameter is not present, the route simply says "Good job!"
+get '/great_job' do
+  name = params[:name]
+  if name
+    "Good job, #{name}!"
+  else
+    "Good job!"
+  end
+end
+# A route that uses route parameters to add two numbers and respond with the result. The data types are tricky here -- when will the data need to be (or arrive as) a string?
+get '/:int1/add/:int2' do
+	first_int = params[:int1].to_i
+	second_int = params[:int2].to_i
+	sum = first_int + second_int
+	"#{first_int} + #{second_int} = #{sum}"
+end
+# Make a route that allows the user to search the database in some way -- maybe for students who have a certain first name, or some other attribute. If you like, you can simply modify the home page to take a query parameter, and filter the students displayed if a query parameter is present.
+get '/:id' do
+  student = db.execute("SELECT * FROM students WHERE id=?",params[:id])[0]
+    response = "Student lookup by ID number: "
+    response << "#{student['id']}<br>"
+    response << "Name: #{student['name']}<br>"
+    response << "Age: #{student['age']}<br>"
+    response << "Campus: #{student['campus']}<br>"
 end
